008798099a575cdff9cabd36d65fdfade4c5eab0
Roman <romanhassan328@gmail.com>
2025-08-22 23:44:28 +0600

    added final

--- final.js
+++ final.js
@@ -62,3 +62,15 @@ console.log(email);
 
 
 //------------06-----------
+/** Problem 06 :  (Current Salary )  */
+var experience = 30;
+var startingSalary = 45000;
+//write your code here
+let currentSalary = startingSalary;
+for(let i=1; i<= experience; i++){
+    // 5% increment per year.
+ incrementSalary =(currentSalary *5 /  100);
+ currentSalary = currentSalary + incrementSalary;
+
+}
+console.log(currentSalary.toFixed(2));
--- problem-06.js
+++ problem-06.js
@@ -2,3 +2,11 @@
 var experience = 30;
 var startingSalary = 45000;
 //write your code here
+let currentSalary = startingSalary;
+for(let i=1; i<= experience; i++){
+    // 5% increment per year.
+ incrementSalary =(currentSalary *5 /  100);
+ currentSalary = currentSalary + incrementSalary;
+
+}
+console.log(currentSalary.toFixed(2));
